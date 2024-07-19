7af6296f7a3bbc1ebf33384f3bb8df1404f749a4
Al3x Ivanov <pro87@yandex.ru>
2024-07-20 03:01:04 +0400

    Created main frame and responsive filters

---
+++ force-app/main/default/lwc/omApp/omApp.js
@@ -0,0 +1,90 @@
+import { LightningElement } from 'lwc';
+
+const items = [
+    {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  {
+    name: 'car',
+    description: 'Description of a car',
+    type: 'Type I',
+    family: 'Family I',
+    price : '$5000',
+    imgUrl : 'https://www.glyffix.com:443/Image/ShowUploadedImage/602',
+    ready: true
+  },
+  
+];
+
+export default class OmApp extends LightningElement {
+    activeFilters;
+}
--- force-app/main/default/lwc/omFilter/omFilter.js
+++ force-app/main/default/lwc/omFilter/omFilter.js
@@ -1,3 +1,64 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
+import { getObjectInfo, getPicklistValuesByRecordType } from 'lightning/uiObjectInfoApi';
+import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
 
-export default class OmFilter extends LightningElement {}
+export default class OmFilter extends LightningElement {
+    fieldNamesForEnabledFilters = ["Type__c", "Family__c"]; //TODO: закастить поле через xml наружу
+    @api activeFilters = [];
+    @track filtersToActivate = [];
+    treeModel;
+    error;
+    recordTypeId;
+
+    @wire(getObjectInfo, { objectApiName: PRODUCT_OBJECT })
+    results({ error, data }) {
+        if (data) {
+            this.recordTypeId = data.defaultRecordTypeId;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.recordTypeId = undefined;
+        }
+    }
+
+    @wire(getPicklistValuesByRecordType, {
+        objectApiName: PRODUCT_OBJECT,
+        recordTypeId: "$recordTypeId"
+    })
+    wiredValues({ error, data }) {
+        if (data) {
+            this.treeModel = this.buildTreeModel(data.picklistFieldValues);
+            this.error = undefined;
+        } else {
+            this.error = error;
+            this.treeModel = undefined;
+        }
+    }
+
+    buildTreeModel(picklistValues) {
+        const treeNodes = [];
+        Object.keys(picklistValues)
+            .filter(item => { if (this.fieldNamesForEnabledFilters.includes(item)) { console.log(item); return true; } })
+            .forEach((picklist) => {
+                treeNodes.push({
+                    label: picklist,
+                    expanded: true,
+                    disabled: false,
+                    items: picklistValues[picklist].values.map((item) => ({
+                        label: item.label,
+                        name: item.value
+                    }))
+                });
+            });
+        return treeNodes;
+    }
+
+    handleSelect(event) {
+        if (this.filtersToActivate.includes(event.detail.name)) {
+            this.filtersToActivate.pop(event.detail.name);
+        }
+        else {
+            this.filtersToActivate.push(event.detail.name);
+        }
+    }
+}
